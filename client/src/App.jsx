6af184c7db5bbedb42401c279ad0a74c43ddd21f
GurkiratSinghArora <sgurkirat868@gmail.com>
2024-03-23 16:22:03 -0700

    Added tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,9 @@ import './App.css'
 
 function App() {
   return (
-    <>Hello World
-    </>
+     <h1 className="text-3xl text-yellow-400 font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
