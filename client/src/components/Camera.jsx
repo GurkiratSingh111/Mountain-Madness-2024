ba7bf45219f6496b0a8c5744b49cdb34eba5354c
GurkiratSinghArora <sgurkirat868@gmail.com>
2024-03-23 22:15:33 -0700

    Added overlay in modal

--- client/src/components/Camera.jsx
+++ client/src/components/Camera.jsx
@@ -1,7 +1,6 @@
 import React, { useRef,useEffect, useState } from 'react';
-import Modal from 'react-modal';
 import CustomModal from './CustomModal';
-
+import Overlay from './Overlay';
 function Camera() {
     const videoRef = useRef(null);
     const canvasRef = useRef(null);
@@ -48,6 +47,7 @@ function Camera() {
             <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow-2xl m-4" >Upload an Image</button>
             <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow-2xl" onClick={openModal}>Click an Image</button>
             {modalIsOpen && <CustomModal closeModal={closeModal}>
+                <Overlay onClick={closeModal} />
                 {!capturedImage &&<video ref={videoRef} autoPlay playsInline></video>}
                 <div className='flex flex-row justify-center'>
                     <button className="bg-black hover:bg-gray-100 text-white font-semibold py-2 px-4 border border-black rounded shadow-2xl m-1" onClick={takePhoto}>Take a photo</button>
