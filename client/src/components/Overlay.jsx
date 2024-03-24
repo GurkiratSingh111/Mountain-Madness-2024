657c415e715c483e851de88650b4fdce986f9c0e
GurkiratSinghArora <sgurkirat868@gmail.com>
2024-03-23 22:07:52 -0700

    Created an overlay component

---
+++ client/src/components/Overlay.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Overlay = () => {
+  return (
+    <div  className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50" onClick={onClick}/>
+  )
+}
+
+export default Overlay
