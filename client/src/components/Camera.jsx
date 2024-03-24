28f6609e268cd382bad296b8bfca5faa509115c5
M-Parsaei <khodadostetdarm@gmail.com>
2024-03-23 22:47:57 -0700

    added the footer and overlay

--- client/src/components/Camera.jsx
+++ client/src/components/Camera.jsx
@@ -47,8 +47,10 @@ function Camera() {
         <div>
             <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow-2xl m-4" >Upload an Image</button>
             <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow-2xl" onClick={openModal}>Click an Image</button>
-            {modalIsOpen && <CustomModal closeModal={closeModal}>
-                <Overlay onClick={closeModal} />
+            {modalIsOpen && 
+            <div className='flex flex-col justify-center items-center'>
+            <Overlay/>
+            <CustomModal closeModal={closeModal}>
                 {!capturedImage &&<video ref={videoRef} autoPlay playsInline></video>}
                 <div className='flex flex-row justify-center'>
                     <button className="bg-black hover:bg-gray-100 text-white font-semibold py-2 px-4 border border-black rounded shadow-2xl m-1" onClick={takePhoto}>Take a photo</button>
@@ -57,7 +59,8 @@ function Camera() {
                
                 {capturedImage && (<img src={capturedImage} className='h w-96' alt="Captured" />
         )}
-            </CustomModal>}
+            </CustomModal>
+            </div>}
         
         <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
     </div>
--- client/src/components/ExamplesPage.jsx
+++ client/src/components/ExamplesPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import Navbar from './Navbar'
+import Footer from './Footer'
 
 const ExamplesPage = () => {
   return (
     <div>
       <Navbar />
         ExamplesPage
+      <Footer/>
     </div>
   )
 }
