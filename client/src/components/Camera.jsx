651089afeeddc9b947c589ab3bf896541876f5a1
GurkiratSinghArora <sgurkirat868@gmail.com>
2024-03-23 23:27:03 -0700

    Completed the camera implementation

--- client/src/components/Camera.jsx
+++ client/src/components/Camera.jsx
@@ -45,24 +45,30 @@ function Camera() {
 
     return (
         <div>
-            <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow-2xl m-4" >Upload an Image</button>
+            <input className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow-2xl m-4"  type="file"
+        accept="image/*" // Allow only image files
+        onChange={capturedImage}/>
             <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow-2xl" onClick={openModal}>Click an Image</button>
             {modalIsOpen && 
             <div className='flex flex-col justify-center items-center'>
             <Overlay/>
             <CustomModal closeModal={closeModal}>
+                <h1 className='text-black text-center mb-1 font-bold'>Click an Image</h1>
                 {!capturedImage &&<video ref={videoRef} autoPlay playsInline></video>}
-                <div className='flex flex-row justify-center'>
-                    <button className="bg-black hover:bg-gray-100 text-white font-semibold py-2 px-4 border border-black rounded shadow-2xl m-1" onClick={takePhoto}>Take a photo</button>
-                    <button className="bg-black hover:bg-gray-100 text-white font-semibold py-2 px-4 border border-black rounded shadow-2xl m-1" onClick={closeModal}>Cancel</button>
-                </div>
-               
                 {capturedImage && (<img src={capturedImage} className='h w-96' alt="Captured" />
         )}
+                <div className='flex flex-row justify-center'>
+                    {!capturedImage && <button className="bg-black hover:bg-gray-100 text-white font-semibold py-2 px-4 border border-black rounded shadow-2xl m-1" onClick={takePhoto}>Take a photo</button>}
+                    {!capturedImage && <button className="bg-black hover:bg-gray-100 text-white font-semibold py-2 px-4 border border-black rounded shadow-2xl m-1" onClick={closeModal} >Cancel</button>}
+                    {capturedImage &&  <button className="bg-black hover:bg-gray-100 text-white font-semibold py-2 px-4 border border-black rounded shadow-2xl m-1" onClick={closeModal}>OK</button>}
+                </div>
             </CustomModal>
             </div>}
-        
+            {capturedImage && (<img src={capturedImage} className='h w-96' alt="Captured" />
+        )}
         <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
+        {capturedImage && <button className="bg-black hover:bg-gray-100 text-white font-semibold py-2 px-4 border border-black rounded shadow-2xl m-1" onClick={closeModal}>Submit</button>}
+        {capturedImage && <button className="bg-black hover:bg-gray-100 text-white font-semibold py-2 px-4 border border-black rounded shadow-2xl m-1" onClick={()=>setCapturedImage(null)}>Delete Image</button>}
     </div>
     );
 }
