ad20f969731f950c0274571bd9bde132616f9064
GurkiratSinghArora <sgurkirat868@gmail.com>
2024-03-23 21:59:19 -0700

    Implemented camera for clicking an image

--- client/src/components/Camera.jsx
+++ client/src/components/Camera.jsx
@@ -3,16 +3,6 @@ import Modal from 'react-modal';
 import CustomModal from './CustomModal';
 
 function Camera() {
-    // const customStyles = {
-    //     content: {
-    //       top: '70%',
-    //       left: '50%',
-    //       right: 'auto',
-    //       bottom: 'auto',
-    //       marginRight: '-50%',
-    //       transform: 'translate(-50%, -50%)',
-    //     },
-    //   };
     const videoRef = useRef(null);
     const canvasRef = useRef(null);
     const [capturedImage, setCapturedImage] = useState(null);
@@ -24,7 +14,6 @@ function Camera() {
         setIsOpen(false);
       }
 
-    // Function to start the camera
     const startCamera = async () => {
         try {
             const stream = await navigator.mediaDevices.getUserMedia({ video: true });
@@ -42,46 +31,19 @@ function Camera() {
             const video = videoRef.current;
             const canvas = canvasRef.current;
             const context = canvas.getContext('2d');
-            // Set canvas dimensions to match video
             canvas.width = video.videoWidth;
             canvas.height = video.videoHeight;
-            // Draw video frame onto canvas
             context.drawImage(video, 0, 0, canvas.width, canvas.height);
-            // Convert canvas content to data URL representing the captured image
             const dataURL = canvas.toDataURL('image/png');
-
-            // Set the captured image in state
             setCapturedImage(dataURL);
         }
         
     };
-
-    // Start the camera when the component mounts
     useEffect(() => {
         startCamera();
     }, [openModal]);
 
     return (
-        // <div>
-        //     <button onClick={openModal}>Open Modal</button>
-        //     <Modal
-        //     style={customStyles}
-        //     isOpen={modalIsOpen}
-        //     onRequestClose={closeModal}
-        //     contentLabel="">
-        //         <video ref={videoRef} playsInline></video>
-        //         <button onClick={takePhoto}>Take Photo</button>
-        //         <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
-        //         <button onClick={closeModal}>close</button>
-        //     </Modal>
-        //     {capturedImage && (
-        //         <div>
-        //             <p>Captured Image:</p>
-        //             <img src={capturedImage} alt="Captured" />
-        //         </div>
-        //     )}
-            
-        // </div>
         <div>
             <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow-2xl m-4" >Upload an Image</button>
             <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow-2xl" onClick={openModal}>Click an Image</button>
